63b45273b720a6b96b10b59d3d7b990a45be814d
Akintade Oluwaseun <akintadeseun816@gmail.com>
2025-04-20 16:35:49 +0100

    Initial Commit

---
+++ vybaze/vybazeApp.swift
@@ -0,0 +1,17 @@
+//
+//  vybazeApp.swift
+//  vybaze
+//
+//  Created by BSTAR on 20/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct vybazeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ vybazeTests/vybazeTests.swift
@@ -0,0 +1,17 @@
+//
+//  vybazeTests.swift
+//  vybazeTests
+//
+//  Created by BSTAR on 20/04/2025.
+//
+
+import Testing
+@testable import vybaze
+
+struct vybazeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
